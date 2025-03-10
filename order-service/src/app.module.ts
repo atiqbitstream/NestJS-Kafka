c29739f3b212dedc38206b44d76e62d957b3f34b
atiqbitstream <atiq.bitstream@gmail.com>
2025-03-10 16:27:45 +0500

    Configure Kafka client module with consumer group in order-service app module

--- order-service/src/app.module.ts
+++ order-service/src/app.module.ts
@@ -1,9 +1,26 @@
+import { Kafka } from './../../user-service/node_modules/kafkajs/types/index.d';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'KAFKA_SERVICE',
+        transport: Transport.KAFKA,
+        options:{
+          client:{
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'order-consumer',
+          }
+        }
+      }
+    ])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
